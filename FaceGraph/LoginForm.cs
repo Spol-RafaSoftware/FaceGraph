0dbc0bbb5dbcbd4f6e118a4e56227fc4fc09a068
Premek Vysoky <premek.vysoky@gmail.com>
2014-12-21 14:36:24 +0100

    Window size fix

--- FaceGraph/LoginForm.cs
+++ FaceGraph/LoginForm.cs
@@ -11,7 +11,11 @@ namespace FaceGraph {
 			InitializeComponent();
 
 			// we get user to the login form
-			webBrowser.Navigate(Config.FACEBOOK_LOGIN + "?response_type=token&client_id=" + Config.APP_ID + "&redirect_uri=" + Config.REDIRECT_URI); 
+			webBrowser.Navigate(Config.FACEBOOK_LOGIN + "?response_type=token"
+                                                      + "&client_id=" + Config.APP_ID
+                                                      + "&redirect_uri=" + Config.REDIRECT_URI
+                                                      + "&scope=read_stream");
+
 			// after every reload we watch whether the login was successful and we got the token
 			// also when there is a login error we have to change the window dimensions so the login form fits
 			webBrowser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser_Loaded);
@@ -42,11 +46,11 @@ namespace FaceGraph {
 			webBrowser.Visible = true;
 			webBrowser.Document.Body.Style = "overflow:hidden";
 
-			// we are at the login page:
-			HtmlElement content = webBrowser.Document.GetElementById("login_form");
-
-			// we center the login form to the form and size it accordingly
-			if(content != null) {
+            HtmlElement content = null;
+			// we are at the login page
+			content = webBrowser.Document.GetElementById("login_form");
+            if(content != null) {
+                // we center the login form to the form and size it accordingly
 				// we get login_form position and size and make our window fit just right on it
 				int xPos = content.OffsetRectangle.Left;
 				int yPos = content.OffsetRectangle.Top;
@@ -61,30 +65,25 @@ namespace FaceGraph {
 				webBrowser.Left = - xPos + 38;
 				webBrowser.Top = - yPos + 76;
 
-				this.Height = content.ScrollRectangle.Height + 160;
+				this.Height = content.ScrollRectangle.Height + 170;
 				this.Width = content.ScrollRectangle.Width + 86;
+
+				this.SizeGripStyle = SizeGripStyle.Hide;
+
+				return;
 			}
 
+			// adjust permission settings
+			// there is no immediate element which we can reference
+			// the dialog shows through JS after page load
+			if(webBrowser.Url.ToString().Contains("/dialog/oauth")) {
+				webBrowser.Left = -375;
+				webBrowser.Top = -155;
 
-			// we are at the application confirmation screen - we need to adjust the size and page position again
-			// same stuff as with the login form
-			content = webBrowser.Document.GetElementById("platform_dialog_content");
-			if(content != null) {
-				int xPos = content.OffsetRectangle.Left;
-				int yPos = content.OffsetRectangle.Top;
-				HtmlElement tempEl = content.OffsetParent;
+				this.Width = 485;
+				this.Height = 420;
 
-				while(tempEl != null) {
-					xPos += tempEl.OffsetRectangle.Left;
-					yPos += tempEl.OffsetRectangle.Top;
-					tempEl = tempEl.OffsetParent;
-				}
-
-				webBrowser.Left = - xPos + 20;
-				webBrowser.Top = - yPos + 20;
-
-				this.Height = content.ScrollRectangle.Height + 70;
-				this.Width = content.ScrollRectangle.Width + 50;
+				this.SizeGripStyle = SizeGripStyle.Show;
 			}			
 		}
 	}
